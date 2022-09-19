9679e4a0e822b1204a1dcbf0b5a026f097794b9e
kzmch-gif <milenakuzmich@gmail.com>
2022-09-19 21:25:02 +0200

    slick-slider, skills, projects DONE

--- script/index.js
+++ script/index.js
@@ -1,36 +1,33 @@
 $('.responsive').slick({
-    dots: true,
     infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay:true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    autoplay: true,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
-  });
+});
