66b16f16164865e9c682d2e7d4a1c146d747bfea
kzmch-gif <milenakuzmich@gmail.com>
2022-09-27 15:14:15 +0200

    adaptive for mobile and ipad version

--- script/index.js
+++ script/index.js
@@ -12,23 +12,26 @@ $('.responsive').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 4,
+                slidesToScroll: 1,
                 infinite: true,
-                dots: true
+    autoplay: true,
+    dots: false
             }
         },
         {
             breakpoint: 600,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+    autoplay: true,
+    slidesToScroll: 1
             }
         },
         {
             breakpoint: 480,
             settings: {
-                slidesToShow: 1,
+    autoplay: true,
+    slidesToShow: 3,
                 slidesToScroll: 1
             }
         }
@@ -40,7 +43,33 @@ $('.projects-slider').slick({
     infinite: true,
     slidesToShow: 3,
     arrows: true,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        } ,
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
 });
 
 //Animation on scroll
